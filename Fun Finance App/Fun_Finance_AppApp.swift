942fde5a94b8abfd2c6a635ec3a6c2aeaf097bca
Griffin Arkilic <garkilic@gmail.com>
2025-09-30 09:05:40 -0700

    Initial Commit

---
+++ Fun Finance App/Fun_Finance_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fun_Finance_AppApp.swift
+//  Fun Finance App
+//
+//  Created by Griffin Arkilic on 9/30/25.
+//
+
+import SwiftUI
+
+@main
+struct Fun_Finance_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
